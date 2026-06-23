8d0eec9f77bca88a71ccefc54097ad06c33504ff
Akhil Mohan <akhilmohan@fakeeh.tech>
2026-06-23 16:21:09 +0530

    added update from github

---
+++ CareerCounsellingApp/Services/UpdateService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Velopack;
+using Velopack.Sources;
+
+namespace CareerCounsellingApp.Services
+{
+    public class UpdateService
+    {
+        private readonly UpdateManager _updateManager;
+
+        public UpdateService()
+        {
+            var source = new GithubSource(
+                "https://github.com/Akhilmohan97/CareerCounsellingApp",
+                null,
+                false);
+
+            _updateManager = new UpdateManager(source);
+        }
+
+        public async Task<UpdateInfo?> CheckForUpdatesAsync()
+        {
+            return await _updateManager.CheckForUpdatesAsync();
+        }
+
+        public async Task DownloadUpdateAsync(
+            UpdateInfo update,
+            Action<int> progress)
+        {
+            await _updateManager.DownloadUpdatesAsync(
+                update,
+                progress);
+
+            _updateManager.ApplyUpdatesAndRestart(update);
+        }
+    }
+}
